54194d152da8ba69b18366a8f8ce35f645a0a58c
ruler31 <boz0987654321@yandex.ru>
2025-03-09 22:11:52 +0300

    last_2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from flask import Flask
 app = Flask(__name__, static_folder="static", template_folder="templates")
+app.secret_key = "yehbbcf83725gfbefuwfew08348gsfe73dwdw2"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run(host='0.0.0.0', port=8888)
+
+if __name__ == '__main__':
+    app.run(app.run(host='0.0.0.0', port=8080))
